de037b169e93045467bbe6b08ccff56becf076e2
Jacob Stern <jacob.stern@outlook.com>
2019-05-03 01:10:00 -0700

    Add diagnostics watcher to dev script

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,9 +1,40 @@
+const chalk = require('chalk');
+const ts = require('typescript');
 const path = require('path');
 const { spawn } = require('child_process');
 const livereload = require('livereload');
 
+const formatHost = {
+  getCanonicalFileName: path => path,
+  getCurrentDirectory: ts.sys.getCurrentDirectory,
+  getNewLine: () => ts.sys.newLine,
+};
+
 async function main() {
   const rootDir = path.resolve(__dirname, '..');
+
+  const configPath = ts.findConfigFile(
+    rootDir,
+    ts.sys.fileExists,
+    'tsconfig.json'
+  );
+
+  if (!configPath) {
+    throw new Error("Could not find a valid 'tsconfig.json'.");
+  }
+
+  // TS watcher hacked together from https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#writing-an-incremental-program-watcher
+  const createProgram = ts.createSemanticDiagnosticsBuilderProgram;
+  const host = ts.createWatchCompilerHost(
+    configPath,
+    {},
+    ts.sys,
+    createProgram,
+    reportDiagnostic,
+    reportWatchStatusChanged
+  );
+  ts.createWatchProgram(host);
+
   const livereloadServer = livereload.createServer({
     exts: ['hbs', 'js'],
     exclusions: ['build/', 'scripts/', '.git/', 'node_modules/'],
@@ -21,6 +52,22 @@ async function main() {
   });
   tsNodeDev.stdout.pipe(process.stdout);
   tsNodeDev.stderr.pipe(process.stderr);
+}
+
+function reportDiagnostic(diagnostic) {
+  console.error(
+    chalk.red(ts.formatDiagnostic(diagnostic, formatHost).trimRight())
+  );
+}
+
+/**
+ * Prints a diagnostic every time the watch status changes.
+ * This is mainly for messages like "Starting compilation" or "Compilation completed".
+ */
+function reportWatchStatusChanged(diagnostic) {
+  if (process.argv.includes('--verbose')) {
+    console.info(ts.formatDiagnostic(diagnostic, formatHost).trimRight());
+  }
 }
 
 main();
